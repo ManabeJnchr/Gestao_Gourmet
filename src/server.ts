756a2f4d22a3db87af31ef60874d0c7afd4484cb
Jean-Luk <jeannfantozzi@gmail.com>
2025-02-19 20:14:12 -0300

    Implementando conexão ao banco de dados

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import express, { Request, Response } from "express";
 import path from "path";
+import pool from './database';
 
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 
 (async function main () {
     app.use(express.static(path.join(__dirname, "..", "public")));
@@ -11,6 +12,27 @@ const PORT = 3000;
         res.sendFile(path.join(__dirname, "..", "public", "html", "index.html"))
     })
 
+    app.get('/testando', async function (req: Request, res: Response) {
+        try {
+            const result = await pool.query('SELECT NOW()')
+            res.json({success: true, time: result.rows[0]})
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({success:false, error: 'Error connecting to database'})
+        }
+    
+    })
+    
+
+    app.get('/funcionario', async function (req: Request, res: Response) {
+        try {
+            const result = await pool.query('SELECT * FROM funcionario')
+            res.json({success: true, time: result.rows})
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({success:false, error: 'Error connecting to database'})
+        }
+    })
     app.listen(PORT, function () {
         console.log(`# Server running on PORT: ${PORT}`)
     })
