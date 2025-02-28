c96409f99524c7e329379bcb561404084ea22eae
Jean-Luk <jeannfantozzi@gmail.com>
2025-02-28 12:49:52 -0300

    Implementado atualizar funcionario

--- src/routes/funcionarioRoutes.ts
+++ src/routes/funcionarioRoutes.ts
@@ -1,8 +1,8 @@
 
 import express from "express";
 
-import { adicionarFuncionario } from '../controllers/funcionarioController';
+import { salvarFuncionario } from '../controllers/funcionarioController';
 
 export default (router: express.Router) => {
-    router.post('/addfuncionario', adicionarFuncionario);
+    router.post('/addfuncionario', salvarFuncionario);
 };
