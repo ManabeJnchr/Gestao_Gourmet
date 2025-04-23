e349c33de761cd236eb305795aff22e8f942a6d3
Jean-Luk <jeannfantozzi@gmail.com>
2025-04-23 13:58:00 -0300

    feat: adicionar rotas para alteração de senha

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,8 +1,10 @@
 import express from "express";
-import { getIdentity, login, logout } from "../controllers/authenticationController";
+import { acceptPasswordReset, getIdentity, login, logout, requestPasswordReset } from "../controllers/authenticationController";
 
 export default (router: express.Router) => {
     router.post('/login', login);
     router.get('/identity', getIdentity);
     router.get('/logout', logout);
+    router.post('/requestPasswordReset', requestPasswordReset);
+    router.post('/acceptPasswordReset', acceptPasswordReset);
 };
