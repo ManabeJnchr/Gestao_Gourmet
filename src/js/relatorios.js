47395f460a007f4ab2cc67a0005c6d58db9ee099
Lucas Samuel <lucasscg.card@gmail.com>
2025-05-23 20:22:06 -0300

    View de relatorio de funcionários / front

--- src/js/relatorios.js
+++ src/js/relatorios.js
@@ -13,10 +13,6 @@ window.RelCardapio = function () {
                 console.log(error);
             });
         },
-        initRelCardapio() {
-            this.listarCategorias();
-            this.inicializarData();
-        },
         inicializarData() {
             const hoje = new Date();
             const padraoData = hoje.toISOString().slice(0, 10);
@@ -34,7 +30,35 @@ window.RelCardapio = function () {
             this.categoria = '';
             this.data_inicial = '';
             this.data_final = '';
-            this.inicializarData();
+        }
+    }
+}
+
+window.RelFuncionario = function () {
+    return {
+        cargo: '',
+        cargos: [],
+        data_inicial: '',
+        data_final: '',
+        rel_funcionario: [],
+        gerarRelatorio() {
+            const filtros = { cargo: this.cargo, data_inicial: this.data_inicial, data_final: this.data_final };
+            axios.post('/gerarRelatorioFuncionario', filtros).then(resp => {
+                this.rel_funcionario = resp.data;
+            }).catch(error => {
+                console.log(error);
+            });
+        },
+        inicializarData() {
+            const hoje = new Date();
+            const padraoData = hoje.toISOString().slice(0, 10);
+            this.data_inicial = padraoData;
+            this.data_final = padraoData;
+        },
+        resetarFiltros() {
+            this.cargo = '';
+            this.data_inicial = '';
+            this.data_final = '';
         }
     }
 }
