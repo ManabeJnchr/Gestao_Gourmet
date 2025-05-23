bf92fb77d7a6127f39fac57d3238ec39251b7bf9
Lucas Samuel <lucasscg.card@gmail.com>
2025-05-23 20:06:45 -0300

    Front da tela de rel cardapio

--- src/js/relatorios.js
+++ src/js/relatorios.js
@@ -1,13 +1,40 @@
-window.RelCadapio = function () {
+window.RelCardapio = function () {
     return {
         categoria: '',
-        data_inicio: '',
-        data_fim: '',
+        categorias: [],
+        data_inicial: '',
+        data_final: '',
+        rel_cardapio: [],
+        gerarRelatorio() {
+            const filtros = { categoria: this.categoria, data_inicial: this.data_inicial, data_final: this.data_final };
+            axios.post('/gerarRelatorioCardapio', filtros).then(resp => {
+                this.rel_cardapio = resp.data;
+            }).catch(error => {
+                console.log(error);
+            });
+        },
         initRelCardapio() {
+            this.listarCategorias();
+            this.inicializarData();
+        },
+        inicializarData() {
             const hoje = new Date();
             const padraoData = hoje.toISOString().slice(0, 10);
-            this.data_inicio = padraoData;
-            this.data_fim = padraoData;
+            this.data_inicial = padraoData;
+            this.data_final = padraoData;
+        },
+        listarCategorias() {
+            axios.get('/listarCategorias').then(resp => {
+                this.categorias = resp.data;
+            }).catch(error => {
+                console.log(error);
+            });
+        },
+        resetarFiltros() {
+            this.categoria = '';
+            this.data_inicial = '';
+            this.data_final = '';
+            this.inicializarData();
         }
     }
 }
