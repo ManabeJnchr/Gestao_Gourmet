5cd28a83b97358e868daef471c0625e81dc8e578
Jean-Luk <jeannfantozzi@gmail.com>
2025-05-25 12:52:29 -0300

    fix: alterar query para listar itens sem pedidos

--- src/services/RelatorioService.ts
+++ src/services/RelatorioService.ts
@@ -20,42 +20,31 @@ class RelatorioService {
 
     static async gerarRelatorioCardapio ({categoria, data_inicial, data_final, exibir_inativo} : RelatorioCardapioParams) {
         try {
-            let query = `
-                SELECT ic.id_itemcardapio,
-                    ic.nome AS nome_item,
-                    c.nome AS categoria,
-                    ic.valor AS valor_atual,
-                    ic.ativo,
-                    COALESCE(SUM(ip.quantidade), 0) AS vezes_pedido,
-                    COALESCE(SUM(ip.valor), 0) AS valor_sem_adicionais,
-                    COALESCE(SUM(aip.valor_adicionais), 0) AS valor_adicionais,
-                    COALESCE(SUM(ip.valor), 0) + COALESCE(SUM(aip.valor_adicionais), 0) AS valor_total_com_adicionais
-                FROM itemcardapio ic JOIN categoria c ON ic.id_categoria = c.id_categoria
-                                    LEFT JOIN itempedido ip ON ic.id_itemcardapio = ip.id_itemcardapio
-                                    LEFT JOIN pedido p ON p.id_pedido = ip.id_pedido
-                                    LEFT JOIN (
-                                                SELECT id_itempedido, SUM(valor) AS valor_adicionais
-                                                FROM adicional_itempedido
-                                                GROUP BY id_itempedido
-                                            ) aip ON aip.id_itempedido = ip.id_itempedido
 
-            `
-
-            const conditions = [];
-            const values = [];
             let paramIndex = 1;
+            const values = []
+
+            const conditionsData = [];
+            let filtroData = '';
 
             if (data_inicial) {
-                conditions.push(`p.data_pedido >= $${paramIndex}`)
+                conditionsData.push(`p.data_pedido >= $${paramIndex}`)
                 values.push(data_inicial);
                 paramIndex++
             }
 
             if (data_final) {
-                conditions.push(`p.data_pedido <= $${paramIndex}`)
+                conditionsData.push(`p.data_pedido <= $${paramIndex}`)
                 values.push(data_final);
                 paramIndex++
             }
+
+            if (conditionsData.length > 0) {
+                filtroData += 'AND ' + conditionsData.join(' AND ');
+            }
+
+            const conditions = [];
+            let filtroWhere = '';
 
             if(categoria) {
                 conditions.push(`ic.id_categoria = $${paramIndex}`)
@@ -68,10 +57,28 @@ class RelatorioService {
             }
 
             if (conditions.length > 0) {
-                query += ` WHERE ` + conditions.join(' AND ');
+                filtroWhere += `WHERE ` + conditions.join(' AND ');
             }
 
-            query += `
+            let query = `
+                SELECT ic.id_itemcardapio,
+                    ic.nome AS nome_item,
+                    c.nome AS categoria,
+                    ic.valor AS valor_atual,
+                    ic.ativo,
+                    COALESCE(SUM(ip.quantidade), 0) AS vezes_pedido,
+                    COALESCE(SUM(ip.valor), 0) AS valor_sem_adicionais,
+                    COALESCE(SUM(aip.valor_adicionais), 0) AS valor_adicionais,
+                    COALESCE(SUM(ip.valor), 0) + COALESCE(SUM(aip.valor_adicionais), 0) AS valor_total_com_adicionais
+                FROM itemcardapio ic JOIN categoria c ON ic.id_categoria = c.id_categoria
+                                    LEFT JOIN itempedido ip ON ic.id_itemcardapio = ip.id_itemcardapio
+                                    LEFT JOIN pedido p ON p.id_pedido = ip.id_pedido ${filtroData}
+                                    LEFT JOIN (
+                                                SELECT id_itempedido, SUM(valor) AS valor_adicionais
+                                                FROM adicional_itempedido
+                                                GROUP BY id_itempedido
+                                            ) aip ON aip.id_itempedido = ip.id_itempedido
+                ${filtroWhere}
                 GROUP BY ic.id_itemcardapio, ic.nome, c.nome, ic.valor, ic.ativo
                 ORDER BY vezes_pedido DESC; 
             `
