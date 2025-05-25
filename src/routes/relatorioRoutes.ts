e964c5e8ad4c0464e6262a162590efbbf4ddf006
Jean-Luk <jeannfantozzi@gmail.com>
2025-05-25 19:38:16 -0300

    feat: adicionar relatório de pedidos

--- src/routes/relatorioRoutes.ts
+++ src/routes/relatorioRoutes.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import { gerarRelatorioCardapio, gerarRelatorioFuncionario } from "../controllers/relatorioController";
+import { gerarRelatorioCardapio, gerarRelatorioFuncionario, gerarRelatorioPedido } from "../controllers/relatorioController";
 
 export default (router: express.Router) => {
     router.post('/gerarRelatorioCardapio', gerarRelatorioCardapio );
     router.post('/gerarRelatorioFuncionario', gerarRelatorioFuncionario );
+    router.post('/gerarRelatorioPedido', gerarRelatorioPedido );
 
 };
