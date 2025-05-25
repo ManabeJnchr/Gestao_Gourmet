6725cdb17c719e36fbd15092582611900e058f3f
Lucas Samuel <lucasscg.card@gmail.com>
2025-05-25 13:44:48 -0300

    Tela de Relatório de Cardapio pornta

--- src/js/relatorios.js
+++ src/js/relatorios.js
@@ -64,3 +64,40 @@ window.RelFuncionario = function () {
         }
     }
 }
+
+window.RelPedido = function () {
+    return {
+        mesa: '',
+        mesas: [],
+        data_inicial: '',
+        data_final: '',
+        rel_cardapio: [],
+        gerarRelatorio() {
+            const filtros = { categoria: this.categoria, data_inicial: this.data_inicial, data_final: this.data_final, exibir_inativo: this.exibir_inativo };
+            axios.post('/gerarRelatorioCardapio', filtros).then(resp => {
+                this.rel_cardapio = resp.data;
+            }).catch(error => {
+                console.log(error);
+            });
+        },
+        inicializarData() {
+            const hoje = new Date();
+            const padraoData = hoje.toISOString().slice(0, 10);
+            this.data_inicial = padraoData;
+            this.data_final = padraoData;
+        },
+        listarCategorias() {
+            axios.get('/listarCategorias').then(resp => {
+                this.categorias = resp.data;
+            }).catch(error => {
+                console.log(error);
+            });
+        },
+        resetarFiltros() {
+            this.categoria = '';
+            this.data_inicial = '';
+            this.data_final = '';
+            this.exibir_inativo = false;
+        }
+    }
+}
